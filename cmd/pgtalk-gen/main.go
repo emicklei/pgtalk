3995242577123ffadca3e49ab1280b43e21c6a96
Ernest Micklei <ernest.micklei@gmail.com>
2021-07-12 09:43:24 +0200

    fix template

--- cmd/pgtalk-gen/main.go
+++ cmd/pgtalk-gen/main.go
@@ -4,12 +4,8 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"html/template"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/jackc/pgx/v4"
 )
@@ -36,84 +32,4 @@ func main() {
 	for _, each := range all {
 		generateFromTable(each)
 	}
-}
-
-func generateFromTable(table PgTable) {
-	log.Println("generating ", table.Name)
-	tt := TableType{
-		Created:    time.Now(),
-		Schema:     *oSchema,
-		TableName:  table.Name,
-		TableAlias: alias(table.Name),
-		GoPackage:  table.Name,
-		GoType:     withoutTrailingS(strings.Title(table.Name)),
-	}
-	for _, each := range table.Columns {
-		goType, method := goFieldTypeAndAccess(each.DataType)
-		f := ColumnField{
-			Name:          each.Name,
-			GoName:        fieldName(each.Name),
-			GoType:        goType,
-			DataType:      each.DataType,
-			FactoryMethod: method,
-		}
-		tt.Fields = append(tt.Fields, f)
-	}
-	tmpl, err := template.New("tt").Parse(tableTemplateSrc)
-	if err != nil {
-		log.Fatal(err)
-	}
-	path := filepath.Join(*oTarget, table.Name)
-	os.MkdirAll(path, os.ModeDir|os.ModePerm)
-	path = filepath.Join(path, "table.go")
-	fileOut, err := os.Create(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fileOut.Close()
-
-	err = tmpl.Execute(fileOut, tt)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func alias(s string) string {
-	return strings.ToLower(s[0:1]) + "1" // TODO
-}
-
-func goFieldTypeAndAccess(datatype string) (string, string) {
-	switch datatype {
-	case "date", "timestamp", "timestamp without time zone", "timestamp with time zone":
-		return "*time.Time", "NewTimeAccess"
-	case "text":
-		return "*string", "NewTextAccess"
-	case "bigint", "integer":
-		return "*int64", "NewInt64Access"
-	case "jsonb":
-		return "*[]byte", "NewBytesAccess"
-	case "point":
-		return "*Point", "NewPointAccess"
-	}
-	if strings.HasPrefix(datatype, "character") {
-		return "*string", "NewTextAccess"
-	}
-	if strings.HasPrefix(datatype, "numeric") {
-		return "*float64", "NewFloat64Access"
-	}
-	return datatype, "New" + datatype
-}
-
-func fieldName(s string) string {
-	if s == "id" {
-		return "ID"
-	}
-	return strings.Title(s)
-}
-
-func withoutTrailingS(s string) string {
-	if strings.HasSuffix(s, "s") {
-		return s[0 : len(s)-1]
-	}
-	return s
 }
